e78cbc5269538e66efeb13fea4e4fdf497937f68
maybeenang <muhammad.120140194@student.itera.ac.id>
2022-10-12 18:24:43 +0700

    Praktikum 2 PemWeb

--- script.js
+++ script.js
@@ -11,21 +11,6 @@ humberger.addEventListener("click", () => {
 
 });
 
-document.addEventListener("click", (e) => {
-
-    for (const n of nav) {
-        if (n.classList.contains("active") && e.target != humberger) {
-            n.classList.remove("active");
-        }
-    }
-
-    const navContainer = document.querySelector(".navContainer");
-
-    if (navContainer.classList.contains("navbar") && e.target != humberger) {
-        navContainer.classList.remove("navbar");
-    }
-});
-
 const navContainer = document.querySelector("nav");
 
 const scroll = document.addEventListener("scroll", (e) => {
